52107cb40862e84e01c4a3f379d371329ebca671
Robert Jackson <rmjackson@gmail.com>
2011-02-08 15:43:10 -0800

    updated

--- ElvUI/layout/gm.lua
+++
@@ -1,6 +0,0 @@
-------------------------------------------------------------------------
---	GM ticket position
-------------------------------------------------------------------------
-
-TicketStatusFrame:ClearAllPoints()
-TicketStatusFrame:SetPoint("TOPLEFT", 250, -5)
--- ElvUI_Dps_Layout/functions.lua
+++
@@ -1,7 +0,0 @@
-local ElvDB = ElvDB
-local ElvCF = ElvCF
-
-if not ElvCF["unitframes"].enable == true and not ElvCF["raidframes"].enable == true then return end
-
-
-ElvDB.LoadUFFunctions("DPS")
