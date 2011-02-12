4e4c2695e0fadb9d76faf9ee6981be0c2f8e52f5
Robert Jackson <rmjackson@gmail.com>
2011-02-12 14:26:40 -0800

    updates DBM
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ DBM-BlackwingDescent/Atramedes.lua
@@ -0,0 +1,108 @@
+local mod	= DBM:NewMod("Atramedes", "DBM-BlackwingDescent", 5)
+local L		= mod:GetLocalizedStrings()
+
+mod:SetRevision(("$Revision: 5068 $"):sub(12, -3))
+mod:SetCreatureID(41442)
+mod:SetZone()
+mod:SetUsedIcons(8)
+
+mod:RegisterCombat("combat")
+
+mod:RegisterEvents(
+	"SPELL_AURA_APPLIED",
+	"SPELL_AURA_REMOVED",
+	"SPELL_CAST_SUCCESS",
+	"CHAT_MSG_MONSTER_YELL"
+)
+
+local warnSonicBreath		= mod:NewSpellAnnounce(78075, 3)
+local warnTracking			= mod:NewTargetAnnounce(78092, 3)
+local warnAirphase			= mod:NewAnnounce("WarnAirphase", 3)
+local warnGroundphase		= mod:NewAnnounce("WarnGroundphase", 3)
+local warnShieldsLeft		= mod:NewAnnounce("WarnShieldsLeft", 3, 77611)
+
+local specWarnSearingFlame	= mod:NewSpecialWarningSpell(77840)
+local specWarnTracking		= mod:NewSpecialWarningYou(78092)
+
+local timerSonicBreath		= mod:NewCDTimer(41, 78075)
+local timerSearingFlame		= mod:NewNextTimer(46.5, 77840)
+local timerAirphase			= mod:NewTimer(90, "TimerAirphase")
+local timerGroundphase		= mod:NewTimer(35, "TimerGroundphase")
+
+local berserkTimer			= mod:NewBerserkTimer(600)
+
+local soundTracking			= mod:NewSound(78092)
+
+mod:AddBoolOption("TrackingIcon")
+mod:AddBoolOption("InfoFrame")
+
+local shieldsLeft = 10
+
+local function groundphase()
+	timerAirphase:Start()
+	timerSonicBreath:Start(25)
+	timerSearingFlame:Start()
+end
+
+function mod:OnCombatStart(delay)
+	timerSonicBreath:Start(25-delay)
+	timerSearingFlame:Start(45-delay)
+	if mod:IsDifficulty("heroic10") or mod:IsDifficulty("heroic25") then
+		berserkTimer:Start(-delay)
+	end
+	shieldsLeft = 10
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:SetHeader(L.Soundlevel)
+		DBM.InfoFrame:Show(5, "power", 30, ALTERNATE_POWER_INDEX)
+	end
+end
+
+function mod:OnCombatEnd()
+	if self.Options.InfoFrame then
+		DBM.InfoFrame:Hide()
+	end
+end 
+
+function mod:SPELL_AURA_APPLIED(args)
+	if args:IsSpellID(78092) then
+		warnTracking:Show(args.destName)
+		if args:IsPlayer() then
+			specWarnTracking:Show()
+			soundTracking:Play()
+		end
+		if self.Options.TrackingIcon then
+			self:SetIcon(args.destName, 8)
+		end
+	end
+end
+
+function mod:SPELL_AURA_REMOVED(args)
+	if args:IsSpellID(78092) then
+		if self.Options.TrackingIcon then
+			self:SetIcon(args.destName, 0)
+		end
+	end
+end
+
+function mod:SPELL_CAST_SUCCESS(args)
+	if args:IsSpellID(78075) then
+		timerSonicBreath:Start()
+		warnSonicBreath:Show()
+	elseif args:IsSpellID(77840) then
+		specWarnSearingFlame:Show()
+--		timerSearingFlame:Start()
+	elseif args:IsSpellID(77611) and not args:IsSrcTypePlayer() then
+		shieldsLeft = shieldsLeft - 1
+		warnShieldsLeft:Show(shieldsLeft)
+	end
+end
+
+function mod:CHAT_MSG_MONSTER_YELL(msg)
+	if msg == L.Airphase or msg:find(L.Airphase)  then
+		warnAirphase:Show()
+		timerSonicBreath:Cancel()
+		timerSearingFlame:Cancel()
+		timerGroundphase:Start()
+		self:Schedule(32.5, groundphase)
+	end
+end
---
+++ DBM-ThroneFourWinds/localization.es.lua
@@ -0,0 +1,65 @@
+﻿if GetLocale() ~= "esES" and GetLocale() ~= "esMX" then return end
+
+local L
+
+------------------------
+--  Conclave of Wind  --
+------------------------
+L = DBM:GetModLocalization("Conclave")
+
+L:SetGeneralLocalization({
+	name = "Cónclave del Viento"
+})
+
+L:SetWarningLocalization({
+	warnSpecial			= "Huracan/Céfiro/Tormenta de granizo activados",
+	specWarnSpecial		= "¡Habilidades especiales activas!"
+})
+
+L:SetTimerLocalization({
+	timerSpecial			= "Habilidades especiales CD",
+	timerSpecialActive		= "Habilidades especiales activas"
+})
+
+L:SetMiscLocalization({
+	gatherstrength			= "%s empieza a extraer fuerza"
+})
+
+L:SetOptionLocalization({
+	warnSpecial			= "Mostrar aviso cuando Huracan/Céfiro/Tormenta de granizo sean lanzados",
+	specWarnSpecial		= "Mostrar aviso especial cuando se lanzan habilidades especiales",
+	timerSpecial		= "Mostrar cooldown de habilidades especiales",
+	timerSpecialActive	= "Mostrar tiempo de duración de habilidades especiales"
+})
+
+---------------
+--  Al'Akir  --
+---------------
+L = DBM:GetModLocalization("AlAkir")
+
+L:SetGeneralLocalization({
+	name = "Al'Akir"
+})
+
+L:SetWarningLocalization({
+	WarnFeedback	= "%s en >%s< (%d)",		-- Feedback on >args.destName< (args.amount)
+})
+
+L:SetTimerLocalization({
+	TimerFeedback 	= "Rebote (%d)",
+	TimerAddCD		= "Siguiente add"
+})
+
+L:SetOptionLocalization({
+	WarnFeedback	= DBM_CORE_AUTO_ANNOUNCE_OPTIONS.spell:format(87904, GetSpellInfo(87904) or "unknown"),
+	LightningRodIcon= DBM_CORE_AUTO_ICONS_OPTION_TEXT:format(89668),
+	TimerFeedback	= "Mostrar tiempo para la duración de $spell:87904",
+	WarnAdd			= "Mostrar un aviso cuando salga una Tormentilla",
+	TimerAddCD		= "Mostrar tiempo para nuevo add"
+})
+
+L:SetMiscLocalization({
+	summonSquall=	"Storms! I summon you to my side!",--translate
+--	phase2		=	"Your futile persistance angers me!",--translate
+	phase3		=	"Enough! I will no longer be contained!"--translate
+})
