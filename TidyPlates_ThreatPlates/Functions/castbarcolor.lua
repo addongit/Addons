a8844ef611c1dcdf8e0e89e4f5e219f5bc75bce2
Robert Jackson <rmjackson@gmail.com>
2011-03-11 20:47:32 -0800

    removed TidyPlates_ThreatPlates for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- TidyPlates_ThreatPlates/Functions/castbarcolor.lua
+++
@@ -1,9 +0,0 @@
-local function SetCastbarColor(unit)
-	local color = {r = "0", g = "0", b = "0", a = "0"}
-	if TidyPlatesThreat.db.profile.castbarColor.toggle then
-		color = TidyPlatesThreat.db.profile.castbarColor
-	end
-	return color.r, color.g, color.b, color.a
-end
-
-TidyPlatesThreat.SetCastbarColor = SetCastbarColor
