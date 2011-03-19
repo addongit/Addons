a48a7687c27276e6422bd8f29a15a977e88e6e5e
Robert Jackson <rmjackson@gmail.com>
2011-03-19 13:15:10 -0700

    Updated XSealBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XSealBar/Localization.de.lua
@@ -0,0 +1,10 @@
+-------------------------------------------------------------------------------
+-- German localization
+-- by Aphelandra - Todeskrallen
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "deDE") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XSealBarButtonToggle:LeftButton"] = "Zeige/Verstecke XSealBar";
+end;
