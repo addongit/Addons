f87371f753a12af41c6846520548f2eb9108d5f7
Robert Jackson <rmjackson@gmail.com>
2011-04-03 16:54:57 -0700

    Updated QuestHelper
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ QuestHelper/bst_post.lua
@@ -0,0 +1,2 @@
+QuestHelper_File["bst_post.lua"] = "4.0.6.176b"
+QuestHelper_Loadtime["bst_post.lua"] = GetTime()
---
+++ QuestHelper/bst_range.lua
@@ -0,0 +1,2 @@
+QuestHelper_File["bst_range.lua"] = "4.0.6.176b"
+QuestHelper_Loadtime["bst_range.lua"] = GetTime()
