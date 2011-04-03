a430bf7eca9780ac86ada528cb39f59680cbc73f
Robert Jackson <rmjackson@gmail.com>
2011-04-03 16:55:02 -0700

    Updated QuestHelper
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ QuestHelper/bst_astrolabe.lua
@@ -0,0 +1,2 @@
+QuestHelper_File["bst_astrolabe.lua"] = "4.0.6.176b"
+QuestHelper_Loadtime["bst_astrolabe.lua"] = GetTime()
---
+++ QuestHelper/bst_ctl.lua
@@ -0,0 +1,2 @@
+QuestHelper_File["bst_ctl.lua"] = "4.0.6.176b"
+QuestHelper_Loadtime["bst_ctl.lua"] = GetTime()
