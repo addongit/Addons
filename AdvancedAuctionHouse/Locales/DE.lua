bb8c422d444ea25d5792aa7269c7d1e9b92f59ba
Robert Jackson <rmjackson@gmail.com>
2011-04-12 08:50:33 +0000

    Updated AdvancedAuctionHouse
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ AdvancedAuctionHouse/Locales/DE.lua
@@ -0,0 +1,233 @@
+AAHLocale = {}
+AAHLocale.Commands = {
+	BARGAIN = "$bargain,$schnäppchen,$sonderangebot,$angebot",
+	DURA = "$dura,$durability,$haltbarkeit",
+	EGGAPTITUDE = "$eggapt,$eibegabung",
+	EGGLEVEL = "$egglvl,$eilevel,$eistufe",
+	FIVE = "$five,$quintuple,$fünf",
+	FOUR = "$four,$quadruple,$vier",
+	GREEN = "$green,$grün",
+	ONE = "$one,$single,$eins",
+	ORANGE = "$orange",
+	PLUS = "$plus",
+	SIX = "$six,$sextuple,$sechs",
+	THREE = "$three,$triple,$drei",
+	TIER = "$tier,$grad",
+	TWO = "$two,$double,$zwei,$doppel",
+	VENDOR = "$vendor,$händler,$verkäufer",
+	YELLOW = "$yellow,$gelb",
+	ZERO = "$zero,$clean,$null",
+}
+AAHLocale.Messages = {
+	ADDON_MANAGER_DESCRIPTION = [=[Advanced AuctionHouse verändert das Standard Auktionshaus-Fenster. Es fügt einige neue Funktionen hinzu, die das Arbeiten mit dem Auktionshaus leichter machen.
+
+Alle Funktionen sind direkt in das Auktionsfenster eingebaut, man braucht also nichts weiter tun als einen Auktionator anzusprechen und das Auktionshaus zu öffnen und kann dann direkt alle Funktionen nutzen.]=],
+	AUCTION_EXCHANGE_RATE = "Wechselkurs",
+	AUCTION_FORUMS_BUTTON = "AAH-Forum",
+	AUCTION_FORUMS_BUTTON_TOOLTIP_HEADER = "Advanced AuctionHouse Foren Webseite",
+	AUCTION_FORUMS_BUTTON_TOOLTIP_TEXT = "Öffnet die Advanced AuctionHouse Foren in deinem Standard-Webbrowser",
+	AUCTION_FRAME_TITLE = "Advanced AuctionHouse <VERSION> von Mavoc (DE)",
+	AUCTION_LOADED_MESSAGE = "Addon geladen: AdvancedAuctionHouse <VERSION> von Mavoc (DE)",
+	BROWSE_CANCELLING = "Der vorherige Suchvorgang wird abgebrochen",
+	BROWSE_CLEAR_BUTTON = "Zurücksetzen",
+	BROWSE_CLEAR_BUTTON_TOOLTIP_HEADER = "Filter zurücksetzen",
+	BROWSE_CLEAR_BUTTON_TOOLTIP_TEXT = "Diesen Knopf drücken um alle Filtereinstellungen zurückzusetzen. Die tatsächlichen Suchergebnisse werden nicht zurückgesetzt.",
+	BROWSE_CREATE_FOLDER_POPUP = "Ordnername erstellen",
+	BROWSE_FILTER = "Filter",
+	BROWSE_FILTER_OR_TOOLTIP_HEADER = "Verknüpfe Suchbegriffe mit ODER",
+	BROWSE_FILTER_OR_TOOLTIP_TEXT = "Markieren um Suchbegriffe mit ODER anstatt mit UND zu verknüpfen.",
+	BROWSE_FILTER_PRICERANGE_BID_TOOLTIP_HEADER = "Bietpreis Filter",
+	BROWSE_FILTER_PRICERANGE_BID_TOOLTIP_TEXT = "Auswählen um nach dem Bietpreis anstatt dem Sofortkaufpreis zu filtern.",
+	BROWSE_FILTER_PRICERANGE_MAX_TOOLTIP_HEADER = "Maximalpreis Filterung",
+	BROWSE_FILTER_PRICERANGE_MAX_TOOLTIP_TEXT = "Gib einen maximalen Preis für den Ergebnisfilter ein.",
+	BROWSE_FILTER_PRICERANGE_MIN_TOOLTIP_HEADER = "Minimalpreis Filterung",
+	BROWSE_FILTER_PRICERANGE_MIN_TOOLTIP_TEXT = "Gib einen minimalen Preis für den Ergebnisfilter ein.",
+	BROWSE_FILTER_PRICERANGE_PPU_TOOLTIP_HEADER = "Preis/Einheit Filterung",
+	BROWSE_FILTER_PRICERANGE_PPU_TOOLTIP_TEXT = "Auswählen um Items anhand des Einzelpreises anstelle des Gesamtpreises zu filtern.",
+	BROWSE_FILTER_TOOLTIP_HEADER = "Filter Suchbegriff #",
+	BROWSE_FILTER_TOOLTIP_TEXT1 = [=[Gib ein Schlüsselwort ein, um die Ergebnisse danach zu filtern.
+Ein ! vor dem Schlüsselwort heißt, dieses darf nicht vorkommen.
+Die Schlüsselwörter der Eingabefelder werden standardmäßig  mit "UND" verknüpft.
+
+Folgende Filter können verwendet werden:]=],
+	BROWSE_FILTER_TOOLTIP_TEXT2 = [=[|cffffd200$green|r - Items mit grünen Werten
+|cffffd200$yellow|r - Items mit gelben Werten
+|cffffd200$orange|r - Items mit orangen Werten
+|cffffd200$zero|r - Items ohne Werte
+|cffffd200$one|r - Items mit einem Wert
+|cffffd200$two|r - Items mit zwei Werten
+|cffffd200$three|r - Items mit drei Werten
+|cffffd200$four|r - Items mit vier Werten
+|cffffd200$five|r - Items mit fünf Werten
+|cffffd200$six|r - Items mit sechs Werten]=],
+	BROWSE_FILTER_TOOLTIP_TEXT3 = [=[|cffffd200$plus|r - Item Upgradestufe Filter
+--($plus/5 Items mit Upgradestufe 5+ anzeigen)
+|cffffd200$dura|r - Item Haltbarkeit Filter
+--($dura/110 Items mit Haltbarkeit 110+ anzeigen)
+|cffffd200$tier|r - Item Grad Filter
+--($tier3 Items mit Grad 3+ anzeigen)
+|cffffd200$egglvl|r - Begleiter-Ei Level Filter
+--($egglvl/30 Begleiter-Ei mit Level 30+ anzeigen)
+|cffffd200$eggapt|r - Begleiter-Ei Begabung Filter
+--($eggapt/80 Begleiter-Ei mit Begabung 80+ anzeigen)
+|cffffd200$vendor|r - An Händler mit Profit verkaufen.
+--(siehe curse.com für mehr Info)
+|cffffd200$bargain|r - Wiederverkauf im AH mit Profit
+--(siehe curse.com für mehr Info)]=],
+	BROWSE_HEADER_CUSTOM_TITLE = "Wähle Auktions-Datentyp:",
+	BROWSE_INFO_LABEL = "<MAXITEMS> Suchergebnisse. <SCANPERCENT>% geladen. Gefilterte Items: <FILTEREDITEMS>. Filterung zu <FILTERPERCENT>% fertig.",
+	BROWSE_INFO_LABEL_TOOLTIP_HEADER = "Zwischenspeicher und Filter-Fortschritt",
+	BROWSE_INFO_LABEL_TOOLTIP_TEXT = [=[Zeigt die folgenden Informationen an:
+
+- Anzahl der Suchergebnisse (max 500)
+- Fortschritt der Suche
+- Treffer der Suchbegriff-Filterung
+- Fortschritt des Filterns]=],
+	BROWSE_MAX = "Max",
+	BROWSE_MIN = "Min",
+	BROWSE_NAME_SEARCH_POPUP = "Gespeicherte Suche benennen",
+	BROWSE_NO_RESULTS = "Die Suche lieferte keine Ergebnisse",
+	BROWSE_OR = "oder",
+	BROWSE_PPU = "PPU",
+	BROWSE_RENAME = "Umbenennen",
+	BROWSE_RENAME_SAVED_SEARCH_POPUP = "Gespeicherte Suche umbenennen",
+	BROWSE_SAVED_SEARCH_TITLE = "Gespeicherte Suchen",
+	BROWSE_SEARCHING = "Suchvorgang gestartet ... einen Moment Geduld bitte",
+	BROWSE_SEARCH_PARAMETERS = "Such Parameter",
+	BROWSE_USABLE = "Verwendbar",
+	GENERAL_ATTRIBUTES = "Attribute",
+	GENERAL_AVERAGE = "Durchschnitt",
+	GENERAL_AVERAGE_PRICE_PER_UNIT = "Durchschnittspreis pro Einheit:",
+	GENERAL_CUSTOM_HEADER_TOOLTIP_HEADER = "Benutzerdefinierte Spalte",
+	GENERAL_CUSTOM_HEADER_TOOLTIP_TEXT = [=[Linksklick: Spalte sortieren
+Rechtsklick: Spalte ändern]=],
+	GENERAL_DECIMAL_POINT = ",",
+	GENERAL_DEX_HEADER = "Gesch", -- Needs review
+	GENERAL_DPS_HEADER = "DPS",
+	GENERAL_DURA_HEADER = "Dura", -- Needs review
+	GENERAL_GENERAL = "Allgemein",
+	GENERAL_HEAL_HEADER = "Heal", -- Needs review
+	GENERAL_HP_HEADER = "HP",
+	GENERAL_INTEL_HEADER = "Int", -- Needs review
+	GENERAL_MACC_HEADER = "M Gen", -- Needs review
+	GENERAL_MATT_HEADER = "M Att", -- Needs review
+	GENERAL_MCRIT_HEADER = "M Crit", -- Needs review
+	GENERAL_MDAM_HEADER = "M Dam", -- Needs review
+	GENERAL_MDEF_HEADER = "M Def", -- Needs review
+	GENERAL_MEDIAN_PRICE_PER_UNIT = "Mittlerer Preis pro Einheit:",
+	GENERAL_MP_HEADER = "MP",
+	GENERAL_OTHER = "Andere", -- Needs review
+	GENERAL_PACC_HEADER = "P Gen", -- Needs review
+	GENERAL_PARRY_HEADER = "Abwehr", -- Needs review
+	GENERAL_PATT_HEADER = "P Att", -- Needs review
+	GENERAL_PCRIT_HEADER = "P Crit", -- Needs review
+	GENERAL_PDAM_HEADER = "P Dam", -- Needs review
+	GENERAL_PDEF_HEADER = "P Def", -- Needs review
+	GENERAL_PDOD_HEADER = "Dodge", -- Needs review
+	GENERAL_PLUS_HEADER = "Plus",
+	GENERAL_PRICE_PER_UNIT_HEADER = "Preis/Einh.",
+	GENERAL_SPEED_HEADER = "Speed", -- Needs review
+	GENERAL_STAM_HEADER = "Ausd", -- Needs review
+	GENERAL_STATS = "Stats", -- Needs review
+	GENERAL_STR_HEADER = "Str", -- Needs review
+	GENERAL_TIER_HEADER = "Grad",
+	GENERAL_WIS_HEADER = "Wis", -- Needs review
+	GENERAL_WORTH_HEADER = "Wert", -- Needs review
+	HISTORY_NO_DATA = "Dieser Gegenstand hat keine Preisentwicklung!",
+	HISTORY_SUMMARY_AVERAGE = [=[Median Preis: <MEDIAN>
+Durchschnitt. Preis: <AVERAGE>]=],
+	HISTORY_SUMMARY_MINMAX = [=[Min. Preis: <MINIMUM>
+Max. Preis: <MAXIMUM>]=],
+	HISTORY_SUMMARY_NUMHISTORY = " (<NUMHISTORY> Auktionen)",
+	LUNA_NEW_VERSION_FOUND = "Eine neuere Version von Advanced Auctionhouse ist verfügbar bei rom.curse.com",
+	LUNA_NOT_FOUND = "Diese Funktion benötigt Luna. Luna kann auf curse gefunden werden.",
+	SELL_AUTO_BID_PRICE_TOOLTIP_HEADER = "Start-Autopreis Modus",
+	SELL_AUTO_BID_PRICE_TOOLTIP_TEXT = [=[Hier den Modus für Start-Autopreis auswählen. Verfügbare Modi sind:
+
+|cffffd200Nichts|r wird automatisch eingetragen
+
+der Preis der |cffffd200letzten|r Auktion des gleichen Items wird verwendet (sofern vorhanden)
+
+|cffffd200Minimum|r Verkaufspreis des Items
+
+|cffffd200Maximum|r Verkaufspreis des Items
+
+|cffffd200Durchschnittlicher|r Verkaufspreis des Items
+
+benutze selbstdefinierte |cffffd200Formel|r
+
+Minimum, Maximum und Durchschnitt ergeben sich aus den letzten 10 Verkäufen (Preisentwicklung).]=],
+	SELL_AUTO_BUYOUT_PRICE_TOOLTIP_HEADER = "Sofortkauf-Autopreis Modus",
+	SELL_AUTO_BUYOUT_PRICE_TOOLTIP_TEXT = [=[Hier den Modus für den Sofortkauf-Autopreis auswählen. Verfügbare Modi sind:
+
+|cffffd200Nichts|r wird automatisch eingetragen
+
+der Preis der |cffffd200letzten|r Auktion des gleichen Items wird verwendet (sofern vorhanden)
+
+|cffffd200Minimum|r Verkaufspreis des Items
+
+|cffffd200Maximum|r Verkaufspreis des Items
+
+|cffffd200Durchschnittlicher|r Verkaufspreis des Items
+
+benutze selbstdefinierte |cffffd200Formel|r
+
+Minimum, Maximum und Durchschnitt ergeben sich aus den letzten 10 Verkäufen (Preisentwicklung).]=],
+	SELL_AUTO_PRICE_FORMULA_TOOLTIP_HEADER = "Benutzerdefinierte Preisformel",
+	SELL_AUTO_PRICE_FORMULA_TOOLTIP_TEXT = [=[Hier eine selbstdefinierte Formel zur Errechnung des Autopreises eintragen. Folgende Platzhalter können benutzt werden:
+
+AVG - Durchschnittspreis
+MEDIAN - Medianpreis
+MAX - Maximalpreis
+MIN - Mindestpreis
+
+Beispiel: AVG - ((AVG - MIN)/3)]=],
+	SELL_AUTO_PRICE_HEADER = "Autopreis Einstellungen",
+	SELL_AUTO_PRICE_PERCENT_TOOLTIP_HEADER = "Preis unterschreiten",
+	SELL_AUTO_PRICE_PERCENT_TOOLTIP_TEXT = "Hier einen %-Wert festlegen, um den der gewählte Autopreis unterschritten wird.",
+	SELL_FORMULA = "Formel",
+	SELL_LAST = "Letzte(r)",
+	SELL_NONE = "Nichts",
+	SELL_NUM_AUCTION = "Anzahl der Auktionen:",
+	SELL_PERCENT = "% unterbieten",
+	SELL_PER_UNIT = "pro Einheit",
+	SETTINGS_BROWSE = "Anzeigeeinstellungen",
+	SETTINGS_CLEAR_ALL_SUCCESS = "Preisentwicklungsdaten erfolgreich zurückgesetzt.",
+	SETTINGS_CLEAR_SUCCESS = "Preisentwicklung erfolgreich gelöscht für: |cffffffff",
+	SETTINGS_FILTER_SPEED = "Filter Geschwindigkeit",
+	SETTINGS_FILTER_SPEED_HEADER = "Filter Geschwindigkeit",
+	SETTINGS_FILTER_SPEED_TEXT = "Setzt die Anzahl der gefilterten Items pro Update. Eine höhere Einstellung bedeutet eine schnellere Filterung, die jedoch das Spiel bremst. Einstellung per Mausrad möglich.",
+	SETTINGS_GENERAL = "Allgemeine Einstellungen",
+	SETTINGS_HISTORY = "History Einstellungen",
+	SETTINGS_MAX_SAVED_HISTORY = "Maximal zu speichernde History",
+	SETTINGS_MAX_SAVED_HISTORY_HEADER = "Maximal zu speichernde History",
+	SETTINGS_MAX_SAVED_HISTORY_TEXT = "Bestimmt die Anzahl der gespeicherten History-Einträge pro Item. Einstellung per Mausrad möglich.",
+	SETTINGS_MISSING_PARAMETER = "Fehlender Parameter zum Löschen der Preisentwicklung.",
+	SETTINGS_PRICE_HISTORY_TOOLTIP = "Preis-History Tooltip immer zeigen",
+	SETTINGS_PRICE_HISTORY_TOOLTIP_HEADER = "Zeige Preis-History Tooltip",
+	SETTINGS_PRICE_HISTORY_TOOLTIP_TEXT = [=[Aktiviert: Zeige Tooltip immer an. Drücke ALT zum ausblenden.
+Deaktiviert: Tooltip wird nur bei gedrückter Alt-Taste angezeigt.]=],
+	SETTINGS_PRICE_PER_UNIT_PER_WHITE = "Preis/Einheit/Weiß für Crafting-Material",
+	SETTINGS_PRICE_PER_UNIT_PER_WHITE_HEADER = "Preis/Einheit/Weiß",
+	SETTINGS_PRICE_PER_UNIT_PER_WHITE_TEXT = [=[Wenn diese Einstellung aktiviert ist, werden alle Crafting-Materialien als Preis pro weißes Item angezeigt. Das soll dir das Vergleichen der Preise von Materialien in verschiedenen Zuständen der Verarbeitung erleichtern. Diese Einstellung ist in allen Bereichen die Preis/Einheit anzeigen wirksam, einschließlich der Verkaufspreise.
+Grün = 2x weißes Material
+Blau = 12x weißes Material
+Violett = 72x weißes Material
+Beispiel:
+100x |cff0072bc[Zink Barren]|r kostet 120,000 für den Stapel
+Preis/Einheit = 1,200
+Preis/Einheit/Weiß = 100]=],
+	TOOLS_DAY_ABV = "t",
+	TOOLS_GOLD_BASED = "(Basierend auf <SCANNED> gescannten Auktionen für Gold)",
+	TOOLS_HOUR_ABV = "s",
+	TOOLS_ITEM_NOT_FOUND = "Keine Daten gefunden für: |cffffffff",
+	TOOLS_MIN_ABV = "m",
+	TOOLS_NO_HISTORY_DATA = "Keine Daten vorhanden.",
+	TOOLS_POWERED_BY = "(powered by Advanced AuctionHouse)",
+	TOOLS_PRICE_HISTORY = "Preisentwicklung",
+	TOOLS_UNKNOWN_COMMAND = [=[Unbekannter Befehl. Befehle sind:
+|cffffffff/aah numhistory <maximale Anzahl der Verlaufseinträge die pro Item gespeichert werden>|r
+|cffffffff/aah clear <item>|r
+|cffffffff/aah clearall|r
+|cffffffff/aah pricehistory|r]=],
+}
