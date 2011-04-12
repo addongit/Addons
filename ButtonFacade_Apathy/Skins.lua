9a30dec8fbd7878eec5e017652e21a0a1a77070c
Robert Jackson <rmjackson@gmail.com>
2011-04-12 08:46:21 +0000

    Updated ButtonFacade_Apathy
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ ButtonFacade_Apathy/Skins.lua
@@ -0,0 +1,98 @@
+--[[ Apathy 4.0.80 ]]
+
+local LBF = LibStub("LibButtonFacade", true)
+if not LBF then return end
+
+-- Apathy
+LBF:AddSkin("Apathy", {
+	Backdrop = {
+		Width = 32,
+		Height = 32,
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Backdrop]],
+	},
+	Icon = {
+		Width = 28,
+		Height = 28,
+	},
+	Flash = {
+		Width = 32,
+		Height = 32,
+		Color = {1, 0, 0, 0.3},
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Overlay]],
+	},
+	Cooldown = {
+		Width = 28,
+		Height = 28,
+	},
+	Pushed = {
+		Width = 32,
+		Height = 32,
+		Color = {0, 0, 0, 0.5},
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Overlay]],
+	},
+	Normal = {
+		Width = 32,
+		Height = 32,
+		Static = true,
+		Color = {0.3, 0.3, 0.3, 1},
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Normal]],
+	},
+	Disabled = {
+		Hide = true,
+	},
+	Checked = {
+		Width = 32,
+		Height = 32,
+		BlendMode = "ADD",
+		Color = {0, 0.8, 1, 0.5},
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Border]],
+	},
+	Border = {
+		Width = 32,
+		Height = 32,
+		BlendMode = "ADD",
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Border]],
+	},
+	Gloss = {
+		Width = 32,
+		Height = 32,
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Gloss]],
+	},
+	AutoCastable = {
+		Width = 54,
+		Height = 54,
+		OffsetX = 0.5,
+		OffsetY = -0.5,
+		Texture = [[Interface\Buttons\UI-AutoCastableOverlay]],
+	},
+	Highlight = {
+		Width = 32,
+		Height = 32,
+		BlendMode = "ADD",
+		Color = {1, 1, 1, 0.5},
+		Texture = [[Interface\AddOns\ButtonFacade_Apathy\Textures\Highlight]],
+	},
+	Name = {
+		Width = 32,
+		Height = 10,
+		OffsetY = -6,
+	},
+	Count = {
+		Width = 32,
+		Height = 10,
+		OffsetX = -2,
+		OffsetY = -6,
+	},
+	HotKey = {
+		Width = 32,
+		Height = 10,
+		OffsetX = -2,
+		OffsetY = 6,
+	},
+	AutoCast = {
+		Width = 26,
+		Height = 26,
+		OffsetX = 1,
+		OffsetY = -1,
+	},
+}, true)
