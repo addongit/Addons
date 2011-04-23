f7904c593c3446e1246b73a02f036587fa9e2a28
Robert Jackson <rmjackson@gmail.com>
2011-04-23 04:32:49 -0700

    removed DBM-Party-BC for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- DBM-Party-BC/CoT_OldHillsbrad/MapSizes.lua
+++
@@ -1,3 +0,0 @@
-DBM:RegisterMapSize("CoTHillsbradFoothills",
-	1, 1668.74990844727, 1112.49987792969
-)
--- DBM-Party-BC/Hellfire_Ramp/MapSizes.lua
+++
@@ -1,3 +0,0 @@
---[[DBM:RegisterMapSize("HellfireRamparts",
-	1, 1355.47009278, 903.647033691
-)--]]
