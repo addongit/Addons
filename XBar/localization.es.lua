aa33ad34bfa2e2d271e7f4f67a393296e9a89f4b
Robert Jackson <rmjackson@gmail.com>
2011-04-26 20:05:54 -0700

    Updated XBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XBar/localization.es.lua
@@ -0,0 +1,38 @@
+-------------------------------------------------------------------------------
+-- Spanish localization
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "esES") then
+
+-- Bindings
+BINDING_NAME_XBAR_CONFIG   = "XBar Configuraci\195\179n";
+
+-- Help text
+XBAR_HELPTEXT1     = "Instrucciones:";
+XBAR_HELPTEXT7     = "Para mover la barra: presione la tecla de control y clic derecha en la XBar";
+XBAR_HELPTEXT8     = "Opciones:";
+
+-- Gui text
+XBAR_CLOSEBTN      = "Cerra";
+XBAR_REVERSE       = "Inversa";
+XBAR_HORIZONTAL    = "Horizontal";
+XBAR_SCALE         = "Escala";
+XBAR_VISIBLE       = "Visible";
+XBAR_TOOLTIPS      = "Tooltips";
+XBAR_SELMOD        = "<-- Seleccionar Barra";
+XBAR_SELOPTION     = "<-- Seleccionar Opci\195\179n";
+XBAR_NOMOD         = "NO BARRAS INSTALADO";
+
+-- Messages
+XBAR_MSG1          = ": Mostrar/esconder la ventana.";
+XBAR_MSG2          = ": No informaci\195\179n de la versi\195\179n encontrada, cambiando a la configuracion por defecto.";
+XBAR_MSG3          = ": Una cambio de la versi\195\179n encontrado, cambiando a la configuracion por defecto.";
+XBAR_MSG4          = "XBar: Inicializado con las opciones ";
+XBAR_MSG5          = ": Addon fall\195\179 al registrar.";
+XBAR_MSG6          = ": Reseteando la posici\195\179n";
+XBAR_MSG7          = "XBar: Opci\195\179n Inv\195\161lida ";
+XBAR_MSG8          = "XBar: Nueva opci\195\179n '";
+XBAR_MSG9          = "' guardado en hueco #";
+XBAR_MSG10         = "XBar: Borrar opci\195\179n: ";
+
+end;
