fc84a06ae8084b35e4ddc283e450d0d84917bb3c
Robert Jackson <rmjackson@gmail.com>
2011-04-27 11:29:36 -0700

    removed XMountBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XMountBar/Localization.es.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- Spanish localization
--------------------------------------------------------------------------------
-
-if (GetLocale() == "esES") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XMountBarButtonToggle:LeftButton"] = "Muestre/Esconde XMountBar";
-end;
