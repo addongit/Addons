70de55968d166504d493699d99c94e57c36ada7b
Robert Jackson <rmjackson@gmail.com>
2011-04-27 11:29:44 -0700

    Updated XWarlockBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XWarlockBar/Localization.de.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- German localization
+-- by Aphelandra - Todeskrallen
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "deDE") then
+
+getfenv()["BINDING_NAME_CLICK XWarlockBarButtonToggle:LeftButton"] = "Zeige/Verstecke XWarlockBar";
+end;
