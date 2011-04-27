7037ba363ecabc4afd6ac98e8d52e4c4c136a232
Robert Jackson <rmjackson@gmail.com>
2011-04-26 20:05:27 -0700

    removed XRogueBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XRogueBar/Localization.fr.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- French localization 
--------------------------------------------------------------------------------
-
-if (GetLocale() == "frFR") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XRogueBarButtonToggle:LeftButton"] = "Montrer/cacher XRogueBar";
-end;
