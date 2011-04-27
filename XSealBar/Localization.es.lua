5985402da53a740afbb7aaa23b8177a9bcf448d8
Robert Jackson <rmjackson@gmail.com>
2011-04-27 11:29:43 -0700

    removed XSealBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XSealBar/Localization.es.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- Spanish localizations
--------------------------------------------------------------------------------
-
-if (GetLocale() == "esES") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XSealBarButtonToggle:LeftButton"] = "Muestre/Esconde XSealBar";
-end;
