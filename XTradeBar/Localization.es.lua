732c251b15511947544d5b9218d946b7bc66721c
Robert Jackson <rmjackson@gmail.com>
2011-04-26 20:05:50 -0700

    Updated XTradeBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XTradeBar/Localization.es.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- Spanish localization
+--
+-------------------------------------------------------------------------------
+if (GetLocale() == "esES") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XTradeBarButtonToggle:LeftButton"] = "Muestre/Esconde XTradeBar";
+end;
