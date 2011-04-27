889a50f5af10824433cf1b03e057a60c68fe8b5f
Robert Jackson <rmjackson@gmail.com>
2011-04-27 11:29:25 -0700

    Updated XRogueBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XRogueBar/Localization.tw.lua
@@ -0,0 +1,9 @@
+﻿-------------------------------------------------------------------------------
+-- Traditional Chinese localization (竹笙姬@暴風祭壇 http://tw.myblog.yahoo.com/norova.tw/)
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "zhTW") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XRogueBarButtonToggle:LeftButton"] = "顯示/隱藏 盜賊Bar";
+end;
