8b23afcbb21cd1883b4d597a30da1145f752644c
Robert Jackson <rmjackson@gmail.com>
2011-04-27 11:29:26 -0700

    removed XCompanionBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XCompanionBar/Localization.fr.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- French localization 
--------------------------------------------------------------------------------
-
-if (GetLocale() == "frFR") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XCompanionBarButtonToggle:LeftButton"] = "Montrer/cacher XCompanionBar";
-end;
