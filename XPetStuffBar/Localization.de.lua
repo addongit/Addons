ca4a179758ad309606f11115d01f4cdb4958e8e2
Robert Jackson <rmjackson@gmail.com>
2011-04-26 20:05:35 -0700

    Updated XPetStuffBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XPetStuffBar/Localization.de.lua
@@ -0,0 +1,10 @@
+-------------------------------------------------------------------------------
+-- German localization
+-- By Aphelandra - Todeskrallen
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "deDE") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XPetStuffBarButtonToggle:LeftButton"] = "Zeige/Verstecke XPetStuffBar";
+end;
