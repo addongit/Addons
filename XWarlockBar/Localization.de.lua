3e900fb343187cec8bf964c02c3caa964fcca454
Robert Jackson <rmjackson@gmail.com>
2011-04-28 14:46:53 -0700

    removed XWarlockBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XWarlockBar/Localization.de.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- German localization
--- by Aphelandra - Todeskrallen
--------------------------------------------------------------------------------
-
-if (GetLocale() == "deDE") then
-
-getfenv()["BINDING_NAME_CLICK XWarlockBarButtonToggle:LeftButton"] = "Zeige/Verstecke XWarlockBar";
-end;
