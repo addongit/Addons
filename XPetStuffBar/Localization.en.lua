c4ba35fc6cf803295aeccd1ec83a07fbc4f0a74c
Robert Jackson <rmjackson@gmail.com>
2011-04-28 14:46:55 -0700

    Updated XPetStuffBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XPetStuffBar/Localization.en.lua
@@ -0,0 +1,37 @@
+-------------------------------------------------------------------------------
+-- English localization (Default)
+-------------------------------------------------------------------------------
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XPetStuffBarButtonToggle:LeftButton"] = "Show/hide XPetStuffBar";
+
+-- Pet Skills
+XPetStuffBarSpells = {
+	"#XP Bar",
+	"*XPBar",
+	"*XPBarText",
+	"*XPBarHorizontal",
+	"#PetStuffBar",
+	"^1515",--Tame Beast
+	"^2641",--Dismiss Pet
+	"^6991",--Feed Pet
+	"^136",--Mend Pet
+	"^982",--Revive Pet
+	"^1462",--Beast Lore
+	"^6197",--Eagle Eye
+	"^19574",--Bestial Wrath
+	"^19577",--Intimidation
+	"^34026",--Kill Command
+	"^53271",--Master's Call
+	"^82692",--Focus Fire
+};
+
+--Eagle eye isn't really a pet ability, but meh, what the hell.
+
+XBAR_HELP_CONTEXT_XPetStuffBar = XBarHelpGUI.ContextHeader("XPetStuffBar").."PetStuffBar spells are in the submenu for PetStuffBar, check a spell to show or uncheck to hide it.\n"..
+  "There is also a pet experience bar, you may configure under the XP Bar submenu:\n"..
+  "* XPBar - Shows/Hides the Pet Experience Bar\n"..
+  "* XPBarText - Shows your pet's experience in numbers on the bar.\n"..
+  "* XPBarHorizontal - Orients the bar horizontally if true, vertically if false.";
+
+XBarCore.Localize(XPetStuffBarSpells);
---
+++ XPetStuffBar/Readme.txt
@@ -0,0 +1,5 @@
+Custom Options:
+
+XPBar:           Show a movable pet XP bar
+XPBarText:       Always show text on the bar.
+XPBarHorizontal: Shows the pet bar Horizontally (uncheck to show vertically)
