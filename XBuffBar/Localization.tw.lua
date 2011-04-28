fe8a0a89ee7af686e2db0cae96cc8f3a9bc08862
Robert Jackson <rmjackson@gmail.com>
2011-04-28 14:46:39 -0700

    removed XBuffBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XBuffBar/Localization.tw.lua
+++
@@ -1,7 +0,0 @@
-﻿-------------------------------------------------------------------------------
--- Traditional Chinese localization (竹笙姬@暴風祭壇 http://tw.myblog.yahoo.com/norova.tw/)
--------------------------------------------------------------------------------
-if (GetLocale() == "zhTW") then
--- Bindings
-getfenv()["BINDING_NAME_CLICK XBuffBarButtonToggle:LeftButton"] = "顯示/隱藏 BuffBar";
-end;
