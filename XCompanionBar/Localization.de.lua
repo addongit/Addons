31f12511f6dafff9666e83ba724a8c9edb60d3d7
Robert Jackson <rmjackson@gmail.com>
2011-04-28 14:46:24 -0700

    removed XCompanionBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XCompanionBar/Localization.de.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- German localization
--- by Aphelandra - Todeskrallen
--------------------------------------------------------------------------------
-
-if (GetLocale() == "deDE") then
-
-getfenv()["BINDING_NAME_CLICK XCompanionBarButtonToggle:LeftButton"] = "Zeige/Verstecke XCompanionBar";
-end;
