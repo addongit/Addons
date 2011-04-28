9402e116ea3446862a26330babbe2bfded8c5820
Robert Jackson <rmjackson@gmail.com>
2011-04-28 14:46:22 -0700

    removed XRogueBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XRogueBar/Localization.es.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- Spanish localization
--------------------------------------------------------------------------------
-
-if (GetLocale() == "esES") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XRogueBarButtonToggle:LeftButton"] = "Muestre/Esconde XRogueBar";
-end;
--- XRogueBar/readme.txt
+++
@@ -1,3 +0,0 @@
-Custom Options:
-
-WrapBar - This will make the bar split up into shorter bars where the Spacers are.  If you want to change the wrap points for this, open Localization.en.lua (or your language) and move the spacers around the list to accomodate.
