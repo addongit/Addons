e2c7c21355471b0a78b973855eaa26b2c0e98631
Robert Jackson <rmjackson@gmail.com>
2011-04-28 14:46:40 -0700

    removed XMountBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XMountBar/Localization.de.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- German localization
--- by Aphelandra - Todeskrallen
--------------------------------------------------------------------------------
-
-if (GetLocale() == "deDE") then
-
-getfenv()["BINDING_NAME_CLICK XMountBarButtonToggle:LeftButton"] = "Zeige/Verstecke XMountBar";
-end;
