b8b3ae053cb727995699a7793152e773b8796673
Robert Jackson <rmjackson@gmail.com>
2011-04-30 00:39:24 -0700

    Updated XSealBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XSealBar/Localization.es.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- Spanish localizations
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "esES") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XSealBarButtonToggle:LeftButton"] = "Muestre/Esconde XSealBar";
+end;
