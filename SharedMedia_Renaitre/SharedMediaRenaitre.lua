f04bd86438b3a09acbcf0a21d72891726d7fd27e
Robert Jackson <rmjackson@gmail.com>
2011-04-29 23:40:15 -0700

    Updated SharedMedia_Renaitre
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ SharedMedia_Renaitre/SharedMediaRenaitre.lua
@@ -0,0 +1,35 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- Background
+LSM:Register("background", "RenaitreFadeBorder", [[Interface\Addons\SharedMedia_Renaitre\background\RenaitreFadeBorder.tga]]) 
+LSM:Register("background", "RenaitreFadeGloss", [[Interface\Addons\SharedMedia_Renaitre\background\RenaitreFadeGloss.tga]]) 
+LSM:Register("background", "RenaitreFadeNormal", [[Interface\Addons\SharedMedia_Renaitre\background\RenaitreFadeNormal.tga]]) 
+
+-- Border
+LSM:Register("border", "RenaitreBeveled10", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreBeveled10.tga]]) 
+LSM:Register("border", "RenaitreBeveled10x", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreBeveled10x.tga]]) 
+LSM:Register("border", "RenaitreBeveled11", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreBeveled11.tga]]) 
+LSM:Register("border", "RenaitreBeveled11x", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreBeveled11x.tga]]) 
+LSM:Register("border", "RenaitreFade10", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade10.tga]]) 
+LSM:Register("border", "RenaitreFade10blk", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade10blk.tga]]) 
+LSM:Register("border", "RenaitreFade10x", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade10x.tga]]) 
+LSM:Register("border", "RenaitreFade10xblk", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade10xblk.tga]]) 
+LSM:Register("border", "RenaitreFade11", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade11.tga]]) 
+LSM:Register("border", "RenaitreFade11blk", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade11blk.tga]]) 
+LSM:Register("border", "RenaitreFade11x", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade11x.tga]]) 
+LSM:Register("border", "RenaitreFade11xblk", [[Interface\Addons\SharedMedia_Renaitre\border\RenaitreFade11xblk.tga]]) 
+
+-- Statusbar
+LSM:Register("statusbar", "RenaitreDefile", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreDefile.tga]]) 
+LSM:Register("statusbar", "RenaitreDefileL", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreDefileL.tga]]) 
+LSM:Register("statusbar", "RenaitreDeviant", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreDeviant.tga]]) 
+LSM:Register("statusbar", "RenaitreDeviantThin", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreDeviantThin.tga]]) 
+LSM:Register("statusbar", "RenaitreDeviantThinL", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreDeviantThinL.tga]]) 
+LSM:Register("statusbar", "RenaitreMinion", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreMinion.tga]]) 
+LSM:Register("statusbar", "RenaitreMinionL", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreMinionL.tga]]) 
+LSM:Register("statusbar", "RenaitreSabrette", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreSabrette.tga]]) 
+LSM:Register("statusbar", "RenaitreSabretteMedium", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreSabretteMedium.tga]]) 
+LSM:Register("statusbar", "RenaitreSabretteWide", [[Interface\Addons\SharedMedia_Renaitre\statusbar\RenaitreSabretteWide.tga]]) 
+
+-- Glow
+LSM:Register("glow", "RenaitreSabretteGlow", [[Interface\Addons\SharedMedia_Renaitre\glow\RenaitreSabretteGlow.tga]]) 
