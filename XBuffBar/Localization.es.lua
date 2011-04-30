3646b6a56816de312e847d768a60d52374ae4ff5
Robert Jackson <rmjackson@gmail.com>
2011-04-29 23:48:50 -0700

    removed XBuffBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XBuffBar/Localization.es.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- Spanish localization (Default)
--------------------------------------------------------------------------------
-
-if (GetLocale() == "esES") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XBuffBarButtonToggle:LeftButton"] = "Muestre/Esconde XBuffBar";
-end;
