bd0df6dce4cf6d9676bb8dfd705b3448afc43e82
Robert Jackson <rmjackson@gmail.com>
2011-04-29 23:49:28 -0700

    Updated XTradeBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XTradeBar/Localization.fr.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- French localization 
+--
+-------------------------------------------------------------------------------
+if (GetLocale() == "frFR") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XTradeBarButtonToggle:LeftButton"] = "Montrer/cacher XTradeBar";
+end;
