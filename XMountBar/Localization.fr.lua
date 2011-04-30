e6d346ffc52fa87efb553e27a80c09818fda52f3
Robert Jackson <rmjackson@gmail.com>
2011-04-30 00:40:34 -0700

    Updated XMountBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XMountBar/Localization.fr.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- French localization 
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "frFR") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XMountBarButtonToggle:LeftButton"] = "Montrer/cacher XMountBar";
+end;
