579983b63836a26a3a7a5d2e4723804ebca09279
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:01:55 -0700

    Updated Decursive
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ Decursive/Localization/esES.lua
@@ -0,0 +1,71 @@
+--[[
+    Decursive (v 2.0) add-on for World of Warcraft UI
+    Copyright (C) 2006-2009 John Wellesz (archarodim AT teaser.fr) ( http://www.2072productions.com/?to=decursive.php )
+
+    This is the continued work of the original Decursive (v1.9.4) by Quu
+    Decursive 1.9.4 is in public domain ( www.quutar.com )
+
+    License:
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU General Public License
+    as published by the Free Software Foundation; either version 2
+    of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+--]]
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
+-- Spanish localization
+-------------------------------------------------------------------------------
+
+--[=[
+--			YOUR ATTENTION PLEASE
+--
+--	   !!!!!!! TRANSLATORS TRANSLATORS TRANSLATORS !!!!!!!
+--
+--    Thank you very much for your interest in translating Decursive.
+--    Do not edit those files. Use the localization interface available at the following address:
+--
+--	################################################################
+--	#  http://wow.curseforge.com/projects/decursive/localization/  #
+--	################################################################
+--
+--    Your translations made using this interface will be automatically included in the next release.
+--
+--]=]
+
+if not DcrLoadedFiles or not DcrLoadedFiles["enUS.lua"] then
+    if not DcrCorrupted then message("Decursive installation is corrupted! (enUS.lua not loaded)"); end;
+    DcrCorrupted = true;
+    return;
+end
+local L = LibStub("AceLocale-3.0"):NewLocale("Decursive", "esES");
+
+if not L then
+    DcrLoadedFiles["esES.lua"] = "2.4.0.2";
+    return;
+end;
+
+L["CLASS_HUNTER"] = "Cazador"
+L["CURSE"] = "Maldición"
+L["DEFAULT_MACROKEY"] = "NONE"
+L["DISEASE"] = "Enfermedad"
+L["MAGIC"] = "Magia"
+L["POISON"] = "Veneno"
+L["STR_CLOSE"] = "Cerrar"
+L["STR_DCR_PRIO"] = "Prioridad decursive"
+L["STR_DCR_SKIP"] = "No decursear"
+L["STR_OPTIONS"] = "Opciones"
+L["STR_OTHER"] = "Otro"
+
+
+
+DcrLoadedFiles["esES.lua"] = "2.4.0.2";
