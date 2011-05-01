f596b82e2e5752f077b75ede802708f180ca082f
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:01:58 -0700

    Updated XMountBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XMountBar/Localization.tw.lua
@@ -0,0 +1,10 @@
+﻿-------------------------------------------------------------------------------
+-- Traditional Chinese localization (竹笙姬@暴風祭壇 http://tw.myblog.yahoo.com/norova.tw/)
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "zhTW") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XMountBarButtonToggle:LeftButton"] = "顯示/隱藏 XMountBar";
+
+end;
