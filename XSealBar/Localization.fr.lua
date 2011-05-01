6c0b9d3faa32d0b5bdc6f7bfa44d69ef0824c0c2
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:09:19 -0700

    removed XSealBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XSealBar/Localization.fr.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- French localization
--------------------------------------------------------------------------------
-
-if (GetLocale() == "frFR") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XSealBarButtonToggle:LeftButton"] = "Montrer/cacher XSealBar";
-end;
