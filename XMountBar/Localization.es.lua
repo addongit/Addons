194079b57e5dcc30ade4e3e5a0e27a0ce4737534
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:09:00 -0700

    Updated XMountBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XMountBar/Localization.es.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- Spanish localization
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "esES") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XMountBarButtonToggle:LeftButton"] = "Muestre/Esconde XMountBar";
+end;
