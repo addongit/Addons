10c83e4b5eaa032dd6f94dc6a275c4f3d8721e51
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:01:46 -0700

    Updated XCompanionBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XCompanionBar/Localization.es.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- Spanish localization
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "esES") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XCompanionBarButtonToggle:LeftButton"] = "Muestre/Esconde XCompanionBar";
+end;
