a19a02328fa93be63505a8b880f67aa690df441c
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:09:18 -0700

    Updated XRogueBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XRogueBar/Localization.fr.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- French localization 
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "frFR") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XRogueBarButtonToggle:LeftButton"] = "Montrer/cacher XRogueBar";
+end;
