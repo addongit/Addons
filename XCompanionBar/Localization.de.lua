0bd3baa74b6ecc4ea9406aaf7350e93348ca66fe
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:03:00 -0700

    Updated XCompanionBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XCompanionBar/Localization.de.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- German localization
+-- by Aphelandra - Todeskrallen
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "deDE") then
+
+getfenv()["BINDING_NAME_CLICK XCompanionBarButtonToggle:LeftButton"] = "Zeige/Verstecke XCompanionBar";
+end;
