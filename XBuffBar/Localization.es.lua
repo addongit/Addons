4a6b492b05b6108f8ff84786ceeff6b4b721d391
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:09:58 -0700

    Updated XBuffBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XBuffBar/Localization.es.lua
@@ -0,0 +1,9 @@
+-------------------------------------------------------------------------------
+-- Spanish localization (Default)
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "esES") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XBuffBarButtonToggle:LeftButton"] = "Muestre/Esconde XBuffBar";
+end;
