8ecc39bedec0d74e749b4ba1789a28063a8afa98
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:02:55 -0700

    removed XTradeBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XTradeBar/Localization.de.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- German localization
--- by Aphelandra - Todeskrallen
--------------------------------------------------------------------------------
-if (GetLocale() == "deDE") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XTradeBarButtonToggle:LeftButton"] = "Zeige/Verstecke XTradeBar";
-end;
