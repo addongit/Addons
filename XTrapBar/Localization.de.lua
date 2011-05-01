50d1002d9193eb1d83824a9e5383a2e50e538483
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:10:12 -0700

    Updated XTrapBar
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

---
+++ XTrapBar/Localization.de.lua
@@ -0,0 +1,10 @@
+-------------------------------------------------------------------------------
+-- German localization
+-- by Aphelandra - Todeskrallen
+-------------------------------------------------------------------------------
+
+if (GetLocale() == "deDE") then
+
+-- Bindings
+getfenv()["BINDING_NAME_CLICK XTrapBarButtonToggle:LeftButton"] = "Zeige/Verstecke XTrapBar";
+end;
