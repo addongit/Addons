f37d7c9cbf3b0f1e41a421286222ddcbff88ab9c
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:10:12 -0700

    removed XTrapBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XTrapBar/Localization.fr.lua
+++
@@ -1,9 +0,0 @@
--------------------------------------------------------------------------------
--- French localization 
--------------------------------------------------------------------------------
-
-if (GetLocale() == "frFR") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XTrapBarButtonToggle:LeftButton"] = "Montrer/cacher XTrapBar";
-end;
