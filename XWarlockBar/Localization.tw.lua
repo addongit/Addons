1f7e98aa62194d86a94456fb418034e71bff0632
Robert Jackson <rmjackson@gmail.com>
2011-04-30 21:10:11 -0700

    removed XWarlockBar for update
    Signed-off-by: Robert Jackson <rmjackson@gmail.com>

--- XWarlockBar/Localization.tw.lua
+++
@@ -1,8 +0,0 @@
-﻿-------------------------------------------------------------------------------
--- Traditional Chinese localization (竹笙姬@暴風祭壇 http://tw.myblog.yahoo.com/norova.tw/)
--------------------------------------------------------------------------------
-if (GetLocale() == "zhTW") then
-
--- Bindings
-getfenv()["BINDING_NAME_CLICK XWarlockBarButtonToggle:LeftButton"] = "顯示/隱藏 術士Bar";
-end;
